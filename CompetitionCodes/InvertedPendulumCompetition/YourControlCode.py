ce4942ff5bd43b4aa1c280012a9ba694c4abe90b
Julia Farber <150543655+JuliaFarber@users.noreply.github.com>
2025-11-26 06:22:07 -0500

    Update YourControlCode.py
    Attempted to get state function, and then the jacobian related set up and dot product.

--- CompetitionCodes/InvertedPendulumCompetition/YourControlCode.py
+++ CompetitionCodes/InvertedPendulumCompetition/YourControlCode.py
@@ -9,56 +9,6 @@ from scipy.linalg import inv, eig
 #velocity, and acceleration of the joints in global frame
 #things like d.body_pos are localy framed
 
-#chat gpt's pseudocode for pendulum balancing:
-'''
-def CtrlUpdate(self):
-    # 1) compute pendulum vector and angle
-    p_hand = self.d.xpos[hand_body_id]   # 3-vector
-    p_tip  = self.d.xpos[pole_tip_body_id]
-    v = p_tip - p_hand                   # pole vector pointing from hand to tip
-
-    # theta: small-angle about x axis in the x-z plane (x horizontal, z up)
-    theta = math.atan2(v[0], v[2])
-    # differentiate theta (filtered)
-    if not hasattr(self, "theta_prev"):
-        self.theta_prev = theta
-        self.t_prev = 0.0
-        self.theta_dot = 0.0
-    dt = self.m.opt.timestep
-    raw_theta_dot = (theta - self.theta_prev) / dt
-    # simple filter
-    alpha = 0.2
-    self.theta_dot = alpha*raw_theta_dot + (1-alpha)*self.theta_dot
-    self.theta_prev = theta
-
-    # 2) desired pivot acceleration (linear controller or LQR)
-    kp_pend = 120.0   # tune these
-    kd_pend = 20.0
-    xdd_des = -kp_pend * theta - kd_pend * self.theta_dot
-
-    # clamp
-    xdd_des = max(min(xdd_des, 50.0), -50.0)
-
-    # 3) desired force at hand (mass of pole m_p)
-    F_x = pole_mass * xdd_des
-    F = np.array([F_x, 0.0, 0.0])
-
-    # 4) map to joint torques: tau = J^T * F
-    J = compute_translational_jacobian(self.m, self.d, hand_body_id)  # 3 x n
-    tau_task = J.T.dot(F)   # n vector
-
-    # 5) gravity compensation (simple)
-    tau_g = compute_gravity_compensation(self.m, self.d)  # n vector, use MuJoCo function if available
-
-    # 6) final torques + saturation
-    tau = tau_task + tau_g
-    tau = np.clip(tau, -max_tau, max_tau)
-
-    self.d.ctrl[:len(tau)] = tau[:self.m.nu]
-    return True
-
-'''
-
 #pseudocode for LQR
 """
 def CtrlUpdate(self):
@@ -114,21 +64,79 @@ def CtrlUpdate(self):
     self.d.ctrl[:len(tau)] = tau[:self.m.nu]
     return True
 """
+
 class YourCtrl:
-  def __init__(self, m:mujoco.MjModel, d: mujoco.MjData):
-    self.m = m
-    self.d = d
-    self.init_qpos = d.qpos.copy()
+    def __init__(self, m:mujoco.MjModel, d: mujoco.MjData):
+        self.m = m
+        self.d = d
+        self.init_qpos = d.qpos.copy()
 
-    # Control gains (using similar values to CircularMotion)
-    self.kp = 50.0
-    self.kd = 3.0
+        # Control gains (using similar values to CircularMotion)
+        self.kp = 50.0
+        self.kd = 3.0
 
+        self.dt = self.m.opt.timestep # might need altering later 
 
-  def CtrlUpdate(self):
-    for i in range(6):
-       self.d.ctrl[i] = 150.0*(self.init_qpos[i] - self.d.qpos[i])  - 5.2 *self.d.qvel[i]
-    return True 
+        self.pendulum_body_id = self.m.body("pendulum").id
+        self.pendulum_joint_id = self.m.joint("pend_roll").id
+        self.hand_body_id = self.m.body("EE_Frame").id
 
+        self.pj_pos_idx = self.m.jnt_qposadr[self.pendulum_joint_id]
+        self.pj_vel_idx = self.m.jnt_dofadr[self.pendulum_joint_id]
 
+        self.K = self.LqrGain()
+
+    def GetState(self): #get theta, theta dot of pendulum #returns a tuple
+        th = self.d.qpos[self.pj_pos_idx] # is this right?
+        th_dot = self.d.qvel[self.pj_vel_idx]
+        return np.array([th, th_dot])
+
+    def LqrGain(self, v):
+        k = 0
+        return k 
+    
+    def gravityComp(self):
+        return 0
+
+    def CtrlUpdate(self):
+        # 0) constants (set elsewhere)
+        #l = pole_length
+        #m_p = pole_mass
+        ## 1) read pendulum state vector make into np.array? ok update that
+        x = self.GetState()
+        ## 3) LQR gain K (precomputed offline or compute once in init)
+        # u_des is desired hand horizontal acceleration
+        u_des = -self.K.dot(x) # u = - K * x
+
+        ## saturate acceleration
+        #u_des = np.clip(u_des, -amax, amax)
+        u_des = np.clip(u_des, -50, 50)
+        ## 4) desired hand force
+        F_hand = np.array([u_des, 0, 0]) 
+        # todo should this be u_des or u_Des multipleid with mass of p?
+        ## 5) Jacobian J_trans (3 x n). Use MuJoCo helper or finite diff if needed.
+        #J = compute_translational_jacobian(self.m, self.d, hand_body_id)
+        point = np.zeros(3) # attached to body so offset from body
+        jacr = None # omega focused so orientation/angular stuff 
+        jacp = np.zeros((3, self.m.nv)) # linear velocity focus?
+        mujoco.mj_jac(self.m, self.d, jacp, jacr, point, self.hand_body_id)
+        # returns void
+
+        ## 6) task torques (simple map)
+        #tau_task = J.T.dot(F_hand)
+        tau_task = jacp.T.dot(F_hand)
+        ## 7) gravity compensation (MuJoCo can compute it; if not approximate)
+        #tau_g = compute_gravity_torques(self.m, self.d)  # size n
+        self.gravityComp() #TODO left off here
+        ## 8) posture (nullspace) PD to hold init_qpos, low gain
+        #kp_null = 5.0
+        #kd_null = 0.5
+        #q_err = self.init_qpos - self.d.qpos
+        #tau_null = kp_null * q_err - kd_null * self.d.qvel
+        ## project nullspace: in practice you can add a small tau_null scaled, or compute nullspace projector
+        ## 9) final torque and limits
+        #tau = tau_task + tau_g + 0.05 * tau_null
+        #tau = np.clip(tau, -tau_max, tau_max)
+        #self.d.ctrl[:len(tau)] = tau[:self.m.nu]
+        return True
 
